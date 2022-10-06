a0849d108318c49152ddaa332a2d6e5667e9397c
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-10-06 13:11:12 +0700

    status game

--- src/components/organism/game/index.tsx
+++ src/components/organism/game/index.tsx
@@ -7,19 +7,33 @@ import {
  } from '../../assembleComponent'
 export default function Game() {
     let [score,setScore]=useState(0);
-        useEffect(()=>{
-          const updateScore=()=>{
-                setScore(score++);
-           }
-        },[score])
+    let [status,setStatus]=useState('');
+    useEffect(()=>{
+            const statusUpdate=()=>{
+                if(score <= 200){
+                    setStatus('yowai mono!')
+                }
+                if(score >= 300){
+                    setStatus('not bad noobs!')
+                }
+                if(score >= 500){
+                    setStatus('not yet mature!')
+                }
+                if(score >= 1000){
+                    setStatus('you are sambal!')
+                }
+            }
+            statusUpdate();
+
+        },[status])//only run when score is change [dependencies]
     const addScore=()=>{
-        
+       return setScore(score++);
     }
   return (
     <div className="d-flex justify-content-between align-items-center">
         <div className="container">
-            <h5 className="fs-5">
-                score:{score}
+            <h5 className="mx-5 my-4 fs-5">
+                score:{`${score} ${status}`}
             </h5>
             <Button 
                   href={'#/game'}
