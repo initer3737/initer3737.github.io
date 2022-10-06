6845be19ee1f08e8b9324eef9dd4a98f9b3b3e93
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-10-06 23:06:56 +0700

    add songs and lirics

--- src/components/organism/lyricSong/index.tsx
+++ src/components/organism/lyricSong/index.tsx
@@ -356,103 +356,116 @@ export default function LyricSong() {
       <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
           <div className="text-center ">
             <h5>
-              <Icon variant={''} icon={'pencil-square'} name={' '}/> 歌詞
+              <Icon variant={''} icon={'pencil-square'} name={' '}/> リテラチュア
             </h5>
           </div>
           <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
-            今、静かな夜の中で
-            無計画に車を走らせた
-            左隣、あなたの
-            横顔を月が照らした
-            ただ、思い出を探る様に
-            辿る様に言葉を繋ぎ合わせれば
-            どうしようもなく溢れてくる
-            日々の記憶
-            あなたのそばで生きると決めたその日から
-            少しずつ変わり始めた世界
-            強く在るように弱さを隠すように
-            演じてきた日々に
-            ある日突然現れたその眼差しが
-            知らなかったこと教えてくれた
-            守るべきものがあればそれだけで
-            こんなにも強くなれるんだ
-            深い深い暗闇の中で
-            出会い、共に過ごしてきた
-            類の無い日々
-            心地よかった
-            いや、幸せだった
-            確かにほら
-            救われたんだよ
-            あなたに
-            わずかな光を捉えて輝いたのは
-            まるで流れ星のような涙
-            不器用な命から流れて零れ落ちた
-            美しい涙
-            強く大きな体に秘めた優しさも
-            どこか苦しげなその顔も
-            愛しく思うんだ
-            姿形じゃないんだ
-            やっと気付いたんだ
-            無情に響く銃声が夜を引き裂く
-            別れの息吹が襲いかかる
-            刹那に輝いた無慈悲な流れ星
-            祈りはただ届かずに消えた
-            この、手の中で燃え尽きた
-            金色の優しい彗星を
-            美しいたてがみを
-            暗闇の中握り締めた
+          彩られていけば幻想が
+          形あるものになるように
+          描いて行ける　叶えて行けるんだ3
+          優しく吹いた風が
+          古いページめくるように
+          振り返るけど　ううんいいのよ
+          知らないことだらけの
+          出会い別れの話
+          滲むインクをそっとなぞった
+          どこ行くの　少し遠くまで
+          置いてきたものは　夢に
+          好きだから選ぶ　選びながら
+          私になっていく
+          また会いましょう　約束だから
+          あなたはそう微笑んだ
+          また会いましょう　小指のまじない
+          誰かの声がして　目が覚めた
+          期待されていること
+          見向きさえされないこと
+          どちらがいいの　ううんどちらも
+          嬉しいし不安だし
+          私だって必死だし
+          主人公になれていますか
+          雨が降る　ひとつ、ひとつずつ
+          誰もいない世界みたい
+          満月を見ないふりしながら
+          明日を待っている
+          信じるだけで　叶えられるわ
+          ひとりはそう背を押した
+          信じるだけで　助けられるわ
+          ひとりはそうまるで願うように
+          やがて　青い空の上で
+          星を　笑う
+          本で見たような夜だった
+          雨は止み頬を伝う
+          朝が来た　忘れないでいてね
+          旅のリテラチュア
+          どこ行くの　少し遠くまで
+          置いてきたものは　夢に
+          好きだから選ぶ　選びながら
+          私になっていく
+          また会いましょう　約束だから
+          あなたはそう微笑んだ
+          また会いましょう　小指のまじない
+          誰かを信じてもいいのかな
           </div>
         </div>
 
     <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
       <div className="text-center ">
         <h5>
-        <Icon variant={''} icon={'pencil-square'} name={' '}/>yasashi suisei
+        <Icon variant={''} icon={'pencil-square'} name={' '}/>Literature [reina ueda]
         </h5>
       </div>
       <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
-          Sekarang, di malam yang tenang
-          Kita menjalankan mobil tanpa tujuan
-          Di sebelah kiriku, 
-          Rembulan menerangi wajahmu
-          Seperti mencari kenangan,
-          Seperti kita mengikutinya, jika kita hanya menghubungkan kata-kata
-          Kenangan di hari-hari itu,
-          Meluap tak terelakkan
-          Sejak hari itu, aku memutuskan 'tuk menjalani hidupku di sisimu
-          Dunia mulai berubah sedikit demi sedikit
-          Seolah menyembunyikan kelemahanku dan menggambarkan diriku kuat,
-          Di hari-hari itu
-          Tatapan matamu yang muncul secara tiba-tiba di hari itu
-          Mengajariku sesuatu yang tak ku ketahui
-          Jika ada sesuatu yang harus dilindungi, hanya dengan itu saja
-          Aku bisa menjadi sekuat diriku yang sekarang
-          Di dalam kegelapan yang sangat dalam
-          Kita bertemu, dan menghabiskan waktu bersama
-          Hari-hari yang tak tergantikan
-          Itu menyenangkan
-          Tidak, itu adalah kebahagian
-          Tentu kau tahu
-          Aku telah diselamatkan
-          Olehmu
-          Menangkap cahaya samar yang bersinar
-          Air mata bagaikan bintang jatuh
-          Mengalir dan tumpah dari kehidupan yang canggung
-          Air mata yang indah
-          Bahkan kebaikan yang tersembunyi di tubuhmu yang kuat dan besar
-          Bahkan kelihatanya kau menderita di suatu tempat
-          Ku sangat memikirkannya
-          Ini bukan tentang penampilan
-          Akhirnya ku menyadarinya
-          Tembakan yang bergema dengan kejam sepanjang malam
-          Bintang jatuh kejam yang bersinar dalam sekejap
-          Doa kita hilang begitu saja tanpa pernah mencapainya
-          Komet lembut yang berwarna emas,
-          Terbakar di tanganku ini
-          Surai yang indah,
-          Aku memegangnya erat-erat di dalam kegelapan
+          Jika dapat terwarnai maka itu adalah ilusi
+          Agar tak menjadi sesuatu yang berbentuk
+          Aku akan melukisnya dan juga mewujudkannya
+          Angin yang berhembus dengan lembut
+          Bagaikan membalik lembaran yang lama
+          Aku berbalik badan
+          "Um, aku baik-baik saja"
+          Penuh dengan hal yang tak diketahui
+          Cerita tentang pertemuan dan perpisahan
+          Perlahan aku melukis dengan tinta yang buram
+          Ke mana kau pergi?
+          ...Kau terasa sedikit jauh
+          Sesuatu yang kutinggalkan ada di mimpi
+          Sementara memilih sesuatu yang disukai
+          Aku menjadi diriku sendiri
+          "Mari kita bertemu lagi, aku berjanji padamu"
+          Ya, kau pun tersenyum kepadaku
+          "Mari kita bertemu lagi", sihir jari kelingking
+          Suara seseorang itu pun membangunkanku
+          Sesuatu yang selalu kuharapkan
+          Sesuatu yang bahkan tak pernah kulihat
+          Mana yang lebih baik?
+          "Um, mungkin keduanya"
+          Aku merasa bahagia dan khawatir
+          Aku juga pernah merasa putus asa
+          Apakah aku bisa menjadi pemeran utama?
+          Hujan turun dengan tetes air, satu per satu
+          Bagaikan dunia tanpa ada seorang pun
+          Sambil berpura-pura tak melihat bulan purnama
+          Aku menunggu hari esok
+          "Pasti terwujud jika terus mempercayainya"
+          Seseorang memberikan dukungannya
+          "Pasti terbantu jika terus mempercayainya"
+          Bagaikan seseorang yang berharap kepadaku
+          Akhirnya di atas langit yang biru itu
+          Bintang-bintang pun tersenyum
+          Seperti malam yang kulihat di dalam buku
+          Hujan berhenti dan membasahi pipiku
+          Pagi datang, jangan pernah melupakannya
+          Literatur petualangan
+          Ke mana kau pergi?
+          ...Kau terasa sedikit jauh
+          Sesuatu yang kutinggalkan ada di mimpi
+          Sementara memilih sesuatu yang disukai
+          Aku menjadi diriku sendiri
+          "Mari kita bertemu lagi, aku berjanji padamu"
+          Ya, kau pun tersenyum kepadaku
+          "Mari kita bertemu lagi", sihir jari kelingking
+          Suara seseorang itu pun membangunkanku
           <div className="mt-3">
-            <VideoFromYoutube src={'https://youtube.com/embed/VyvhvlYvRnc'} />
+            <VideoFromYoutube src={'https://youtube.com/embed/7u9MtqfcWaU'} />
           </div>
       </div>
     </div>
@@ -467,131 +480,100 @@ export default function LyricSong() {
     <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
           <div className="text-center ">
             <h5>
-              <Icon variant={''} icon={'pencil-square'} name={' '}/> Коробейники
+              <Icon variant={''} icon={'pencil-square'} name={' '}/> Служить России
             </h5>
           </div>
           <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
-          Ой, полным, полна моя коробушка
-          Есть и ситец, и парча.
-          Пожалей, душа моя, зазнобушка,
-          Молодецкого плеча!
-          Пожалей, душа моя, зазнобушка,
-          Молодецкого плеча!
-          Выйдy, выйдy в рожь высокую
-          Там до ночки погожу.
-          А завижу черноокую
-          Все товары разложу.
-          А завижу черноокую
-          Все товары разложу.
-          Цены сам платил немалые,
-          Не торгуйся, не скупись:
-          Подставляй-ка губы алые,
-          Ближе к молодцу садись.
-          Подставляй-ка губы алые,
-          Ближе к молодцу садись.
-          Вот уж пала ночь туманная,
-          Ждет удалый молодец.
-          Вот, идет- пришла желанная,
-          Продает товар купец.
-          Вот, идет- пришла желанная,
-          Продает товар купец.
-          Девка бережно торгуется
-          Все боится передать.
-          Парень с девицей целуется
-          Просит цену набавлять.
-          Парень с девицей целуется
-          Просит цену набавлять.
-          Знает только ночь глубокая
-          Как поладили они.
-          Распрямись ты, рожь высокая,
-          Тайну свято сохрани!
-          Распрямись ты, рожь высокая,
-          Тайну свято сохрани!
-          Ой! Легка, легка коробушка,
-          Плеч не режет ремешок!
-          А всего взяла зазнобушка
-          Бирюзовый перстенек.
-          А всего взяла зазнобушка
-          Бирюзовый перстенек.
-          Дал ей ситцу штуку целую,
-          Ленту алую для кос.
-          Поясок-рубаху белую
-          Подпоясать в сенокос.
-          Поясок-рубаху белую
-          Подпоясать в сенокос.
-          Все поклала ненаглядная
-          В короб кроме перстенька:
-          "Не хочу ходить нарядная
-          Без сердечного дружка!
-          Не хочу ходить нарядная
-          Без сердечного дружка!"
+          Полки идут стеной,
+          Красиво держат строй
+          И гордо шелестят знамена,
+          Комбат и рядовой - единою судьбой
+          Мы связаны навек, друг мой.
+          Служить России суждено тебе и мне,
+          служить России - удивительной стране,
+          Где солнце новое встает на небе синем
+          Плечо к плечу идут Российские войска,
+          И пусть военная дорога нелегка,
+          Мы будем верою и правдою служить России.
+          В бесстрашии атак
+          Спасли мы Русский флаг
+          И дом родной и наши песни.
+          А коль придет беда, с тобою мы тогда
+          Отчизну защитим, друг мой.
+          Служить России суждено тебе и мне,
+          служить России - удивительной стране,
+          Где солнце новое встает на небе синем
+          Плечо к плечу идут Российские войска,
+          И пусть военная дорога нелегка,
+          Мы будем верою и правдою служить России.
+          Полки идут стеной,
+          красиво держат строй
+          и вместе с нами вся Россия
+          И он, и ты, и я - армейская семья
+          и этим мы сильны, друг мой.
+          Служить России суждено тебе и мне,
+          Служить России - удивительной стране,
+          Где солнце новое встает на небе синем
+          Плечо к плечу идут Российские войска,
+          И пусть военная дорога нелегка,
+          Мы будем верою и правдою служить России.
+          Служить России суждено тебе и мне,
+          Служить России - удивительной стране,
+          Где солнце новое встает на небе синем
+          Плечо к плечу идут российские войска,
+          И пусть военная дорога нелегка,
+          Мы будем... служить... России !
           </div>
         </div>
 
     <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
       <div className="text-center ">
         <h5>
-        <Icon variant={''} icon={'pencil-square'} name={' '}/> korobeiniki / khorobushka /The Peddlers
+        <Icon variant={''} icon={'pencil-square'} name={' '}/> Untuk melayani Rusia
         </h5>
       </div>
       <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
-      Oh, so full, so full is my crate,
-      There's even chintz and brocade.
-      Have mercy, sweetie, my loved one
-      For this lad's shoulder!
-      Have mercy, sweetie, my loved one
-      For this lad's shoulder!
-      Come out, come out into the tall rye,
-      There I will wait until the night,
-      And will see the girl with the dark eyes,
-      showcase all my goods.
-      And will see the girl with the dark eyes,
-      showcase all my goods.
-      Myself I have paid no small price,
-      So don't bargain, don't be stingy,
-      Present your scarlet lips,
-      Sit closer to the lad.
-      Present your scarlet lips,
-      Sit closer to the lad.
-      Now the misty night has come,
-      The daring lad awaits,
-      There she comes! The loved one is here,
-      The merchant will sell his goods.
-      There she comes! The loved one is here,
-      The merchant will sell his goods.
-      Cautiously the lass is haggling,
-      Afraid to give everything away,
-      The guy kisses the girl,
-      Asks for a higher price.
-      The guy kisses the girl,
-      Asks for a higher price.
-      Only the deep night knows,
-      What kind of deal they made.
-      Straighten up now, tall rye,
-      And faithfully keep the secret!
-      Straighten up now, tall rye,
-      And faithfully keep the secret!
-      Oh, how light, how light is my crate,
-      The strap no longer cuts into my shoulders!
-      While all my little one has taken
-      Is a turquoise ring.
-      While all my little one has taken
-      Is a turquoise ring.
-      I had given her a whole piece of calico,
-      A scarlet ribbon for her braids.
-      A little belt -- the white shirt
-      for haymaking
-      A little belt -- the white shirt
-      to strap on while haymaking
-      The sweet one
-      Put everything back into the basket
-      But for the ring:
-      "I do not want to go around dressed up
-      Without a boyfriend!"
-      "I do not want to go around dressed up
-      Without a boyfriend!"
+      Para resimen berjalan seperti dinding yang lurus,
+      Rapi dan teratur,
+      Dan bendera berderau dengan bangga.
+      Komandan dan prajurit - dengan takdir yang yang sama,
+      Kita selamanya akan terkait, temanku.
+      Untuk melayani Rusia adalah takdir untuk kau dan aku,
+      Untuk melayani Rusia - negeri yang luar biasa,
+      Dimana matahari terbit di langit biru.
+      Prajurit Rusia berjalan bahu-membahu,
+      Dan meskipun jalan militer tidaklah mudah,
+      Kita akan melayani Rusia dengan iman dan kebenaran.
+      Dalam keberanian peperangan,
+      Kami telah menyelamatkan bendera Rusia ini,
+      Dan rumah dan lagu kita.
+      Dan jika ada masalah mendatng, kami akan mendampingimu,
+      Kami akan mempertahankan negeri ini, temanku.
+      Untuk melayani Rusia adalah takdir untuk kau dan aku,
+      Untuk melayani Rusia - negeri yang luar biasa,
+      Dimana matahari terbit di langit biru.
+      Prajurit Rusia berjalan bahu-membahu,
+      Dan meskipun jalan militer tidaklah mudah,
+      Kita akan melayani Rusia dengan iman dan kebenaran.
+      Para resimen berjalan seperti dinding yang lurus,
+      Rapi dan teratur,
+      Dan bersama kami, seluruh Rusia ini.
+      Dia, kau, dan saya adalah keluarga militer kita,
+      Dan itu membuat kami kuat, temanku.
+      Untuk melayani Rusia adalah takdir untuk kau dan aku,
+      Untuk melayani Rusia - negeri yang luar biasa,
+      Dimana matahari terbit di langit biru.
+      Prajurit Rusia berjalan bahu-membahu,
+      Dan meskipun jalan militer tidaklah mudah,
+      Kita akan melayani Rusia dengan iman dan kebenaran.
+      Untuk melayani Rusia adalah takdir untuk kau dan aku,
+      Untuk melayani Rusia - negeri yang luar biasa,
+      Dimana matahari terbit di langit biru.
+      Prajurit Rusia berjalan bahu-membahu,
+      Dan meskipun jalan militer tidaklah mudah,
+      Kita akan... melayani... Rusia!
         <div className="mt-3">
-          <VideoFromYoutube src={'https://youtube.com/embed/Y_WBf69oVgQ'} />
+          <VideoFromYoutube src={'https://youtube.com/embed/si_KzQjLtEg'} />
         </div>
       </div>
     </div>
