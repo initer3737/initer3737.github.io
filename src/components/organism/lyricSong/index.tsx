acc7576c54ca19500c5b1aaf52d3c8fd74117cbb
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-10-06 22:18:50 +0700

    add songs

--- src/components/organism/lyricSong/index.tsx
+++ src/components/organism/lyricSong/index.tsx
@@ -3,6 +3,7 @@ import { Icon, Img, VideoFromYoutube } from '../../assembleComponent';
 import Logo1 from '../../../imgs/night-1851685_960_720.png'
 import Logo2 from '../../../imgs/the-background-292720.png'
 import Logo3 from '../../../imgs/wave-305226.png'
+import Logo4 from '../../../imgs/computer-2982270_1920.jpg'
 export default function LyricSong() {
   return (
     <div className='row'>
@@ -185,13 +186,6 @@ export default function LyricSong() {
           </div>
         </div>
 
-    {/* <div className="col-12 bg-1 d-flex align-items-center align-content-center">
-      <Img
-        src={Logo1}
-        className="shadow img-fluid col-12"
-        alt="" srcset={''} width={''} height={''} attr={[]}       
-          />
-    </div> */}
     <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
       <div className="text-center ">
         <h5>
@@ -357,6 +351,250 @@ export default function LyricSong() {
           alt="" srcset={''} width={''} height={''} attr={[]}        
           />
       </div>
+
+      <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
+          <div className="text-center ">
+            <h5>
+              <Icon variant={''} icon={'pencil-square'} name={' '}/> 歌詞
+            </h5>
+          </div>
+          <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
+            今、静かな夜の中で
+            無計画に車を走らせた
+            左隣、あなたの
+            横顔を月が照らした
+            ただ、思い出を探る様に
+            辿る様に言葉を繋ぎ合わせれば
+            どうしようもなく溢れてくる
+            日々の記憶
+            あなたのそばで生きると決めたその日から
+            少しずつ変わり始めた世界
+            強く在るように弱さを隠すように
+            演じてきた日々に
+            ある日突然現れたその眼差しが
+            知らなかったこと教えてくれた
+            守るべきものがあればそれだけで
+            こんなにも強くなれるんだ
+            深い深い暗闇の中で
+            出会い、共に過ごしてきた
+            類の無い日々
+            心地よかった
+            いや、幸せだった
+            確かにほら
+            救われたんだよ
+            あなたに
+            わずかな光を捉えて輝いたのは
+            まるで流れ星のような涙
+            不器用な命から流れて零れ落ちた
+            美しい涙
+            強く大きな体に秘めた優しさも
+            どこか苦しげなその顔も
+            愛しく思うんだ
+            姿形じゃないんだ
+            やっと気付いたんだ
+            無情に響く銃声が夜を引き裂く
+            別れの息吹が襲いかかる
+            刹那に輝いた無慈悲な流れ星
+            祈りはただ届かずに消えた
+            この、手の中で燃え尽きた
+            金色の優しい彗星を
+            美しいたてがみを
+            暗闇の中握り締めた
+          </div>
+        </div>
+
+    <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
+      <div className="text-center ">
+        <h5>
+        <Icon variant={''} icon={'pencil-square'} name={' '}/>yasashi suisei
+        </h5>
+      </div>
+      <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
+          Sekarang, di malam yang tenang
+          Kita menjalankan mobil tanpa tujuan
+          Di sebelah kiriku, 
+          Rembulan menerangi wajahmu
+          Seperti mencari kenangan,
+          Seperti kita mengikutinya, jika kita hanya menghubungkan kata-kata
+          Kenangan di hari-hari itu,
+          Meluap tak terelakkan
+          Sejak hari itu, aku memutuskan 'tuk menjalani hidupku di sisimu
+          Dunia mulai berubah sedikit demi sedikit
+          Seolah menyembunyikan kelemahanku dan menggambarkan diriku kuat,
+          Di hari-hari itu
+          Tatapan matamu yang muncul secara tiba-tiba di hari itu
+          Mengajariku sesuatu yang tak ku ketahui
+          Jika ada sesuatu yang harus dilindungi, hanya dengan itu saja
+          Aku bisa menjadi sekuat diriku yang sekarang
+          Di dalam kegelapan yang sangat dalam
+          Kita bertemu, dan menghabiskan waktu bersama
+          Hari-hari yang tak tergantikan
+          Itu menyenangkan
+          Tidak, itu adalah kebahagian
+          Tentu kau tahu
+          Aku telah diselamatkan
+          Olehmu
+          Menangkap cahaya samar yang bersinar
+          Air mata bagaikan bintang jatuh
+          Mengalir dan tumpah dari kehidupan yang canggung
+          Air mata yang indah
+          Bahkan kebaikan yang tersembunyi di tubuhmu yang kuat dan besar
+          Bahkan kelihatanya kau menderita di suatu tempat
+          Ku sangat memikirkannya
+          Ini bukan tentang penampilan
+          Akhirnya ku menyadarinya
+          Tembakan yang bergema dengan kejam sepanjang malam
+          Bintang jatuh kejam yang bersinar dalam sekejap
+          Doa kita hilang begitu saja tanpa pernah mencapainya
+          Komet lembut yang berwarna emas,
+          Terbakar di tanganku ini
+          Surai yang indah,
+          Aku memegangnya erat-erat di dalam kegelapan
+          <div className="mt-3">
+            <VideoFromYoutube src={'https://youtube.com/embed/VyvhvlYvRnc'} />
+          </div>
+      </div>
+    </div>
+
+       <div className="col-12 bg-1 d-flex align-items-center align-content-center">
+        <Img
+          src={Logo4}
+          className="shadow img-fluid col-12"
+          alt="" srcset={''} width={''} height={''} attr={[]}       
+           />
+      </div>
+    <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
+          <div className="text-center ">
+            <h5>
+              <Icon variant={''} icon={'pencil-square'} name={' '}/> Коробейники
+            </h5>
+          </div>
+          <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
+          Ой, полным, полна моя коробушка
+          Есть и ситец, и парча.
+          Пожалей, душа моя, зазнобушка,
+          Молодецкого плеча!
+          Пожалей, душа моя, зазнобушка,
+          Молодецкого плеча!
+          Выйдy, выйдy в рожь высокую
+          Там до ночки погожу.
+          А завижу черноокую
+          Все товары разложу.
+          А завижу черноокую
+          Все товары разложу.
+          Цены сам платил немалые,
+          Не торгуйся, не скупись:
+          Подставляй-ка губы алые,
+          Ближе к молодцу садись.
+          Подставляй-ка губы алые,
+          Ближе к молодцу садись.
+          Вот уж пала ночь туманная,
+          Ждет удалый молодец.
+          Вот, идет- пришла желанная,
+          Продает товар купец.
+          Вот, идет- пришла желанная,
+          Продает товар купец.
+          Девка бережно торгуется
+          Все боится передать.
+          Парень с девицей целуется
+          Просит цену набавлять.
+          Парень с девицей целуется
+          Просит цену набавлять.
+          Знает только ночь глубокая
+          Как поладили они.
+          Распрямись ты, рожь высокая,
+          Тайну свято сохрани!
+          Распрямись ты, рожь высокая,
+          Тайну свято сохрани!
+          Ой! Легка, легка коробушка,
+          Плеч не режет ремешок!
+          А всего взяла зазнобушка
+          Бирюзовый перстенек.
+          А всего взяла зазнобушка
+          Бирюзовый перстенек.
+          Дал ей ситцу штуку целую,
+          Ленту алую для кос.
+          Поясок-рубаху белую
+          Подпоясать в сенокос.
+          Поясок-рубаху белую
+          Подпоясать в сенокос.
+          Все поклала ненаглядная
+          В короб кроме перстенька:
+          "Не хочу ходить нарядная
+          Без сердечного дружка!
+          Не хочу ходить нарядная
+          Без сердечного дружка!"
+          </div>
+        </div>
+
+    <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
+      <div className="text-center ">
+        <h5>
+        <Icon variant={''} icon={'pencil-square'} name={' '}/> korobeiniki / khorobushka /The Peddlers
+        </h5>
+      </div>
+      <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
+      Oh, so full, so full is my crate,
+      There's even chintz and brocade.
+      Have mercy, sweetie, my loved one
+      For this lad's shoulder!
+      Have mercy, sweetie, my loved one
+      For this lad's shoulder!
+      Come out, come out into the tall rye,
+      There I will wait until the night,
+      And will see the girl with the dark eyes,
+      showcase all my goods.
+      And will see the girl with the dark eyes,
+      showcase all my goods.
+      Myself I have paid no small price,
+      So don't bargain, don't be stingy,
+      Present your scarlet lips,
+      Sit closer to the lad.
+      Present your scarlet lips,
+      Sit closer to the lad.
+      Now the misty night has come,
+      The daring lad awaits,
+      There she comes! The loved one is here,
+      The merchant will sell his goods.
+      There she comes! The loved one is here,
+      The merchant will sell his goods.
+      Cautiously the lass is haggling,
+      Afraid to give everything away,
+      The guy kisses the girl,
+      Asks for a higher price.
+      The guy kisses the girl,
+      Asks for a higher price.
+      Only the deep night knows,
+      What kind of deal they made.
+      Straighten up now, tall rye,
+      And faithfully keep the secret!
+      Straighten up now, tall rye,
+      And faithfully keep the secret!
+      Oh, how light, how light is my crate,
+      The strap no longer cuts into my shoulders!
+      While all my little one has taken
+      Is a turquoise ring.
+      While all my little one has taken
+      Is a turquoise ring.
+      I had given her a whole piece of calico,
+      A scarlet ribbon for her braids.
+      A little belt -- the white shirt
+      for haymaking
+      A little belt -- the white shirt
+      to strap on while haymaking
+      The sweet one
+      Put everything back into the basket
+      But for the ring:
+      "I do not want to go around dressed up
+      Without a boyfriend!"
+      "I do not want to go around dressed up
+      Without a boyfriend!"
+        <div className="mt-3">
+          <VideoFromYoutube src={'https://youtube.com/embed/Y_WBf69oVgQ'} />
+        </div>
+      </div>
+    </div>
+
     </div>
   );
 }
