dc8259e5668016ae58a08c916638643ee2d77382
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-10-07 14:16:23 +0700

    delete image and refactor;

--- src/utils/clock/index.tsx
+++ src/utils/clock/index.tsx
@@ -1,34 +1,40 @@
 import {useState,useEffect} from 'react'
-
+            //time initialize
     let date=new Date();
+    //utils function
+    const validateTime=(T:number)=>T<10?`0${T}`:T;
     const Waktu={
         hari:date.getDay(),
         tanggal:date.getDate(),
         bulan:date.getMonth(),
         tahun:date.getFullYear(),
-    }
-    const validWaktu={
-        hari:['minggu','senin','selasa','rabu','kamis',"jum'at",'sabtu'],
-        bulan:[
-            'januari','februari','maret','april','mei','juni',
-            'juli','agustus','september','oktober','november','desember'
-        ],
-    }
+        validWaktu:{
+            hari:['minggu','senin','selasa','rabu','kamis',"jum'at",'sabtu'],
+            bulan:[
+                'januari','februari','maret','april','mei','juni',
+                'juli','agustus','september','oktober','november','desember'
+            ],
+        }     
+    } 
+        //return date
     const tanggal=()=>{
-        const {tahun,bulan,hari,tanggal}=Waktu;
-        const fulldate=`${validWaktu.hari[hari]} ${validateTime(tanggal)} ${validWaktu.bulan[bulan]} ${tahun}`;
+        const {tahun,bulan,hari,tanggal,validWaktu}=Waktu;
+        const fulldate=`${validWaktu.hari[hari]} ${tanggal} ${validWaktu.bulan[bulan]} ${tahun}`;
         return fulldate;
-    };
-    const validateTime=(T:number)=>T<10?`0${T}`:T;
+    };    
+        //function component [main function]
 export default function Clock() {
-    let time=date.toLocaleTimeString();
-    const [clock,setClock]=useState(time);
-            const updateClock=()=>{
-                let time=new Date().toLocaleTimeString();
-                setClock(time)
-            };
+    let times=new Date().toLocaleTimeString()
+    //states
+    let [time,setTime]=useState(times)   
+    //functions to update the clock
+    const updateClock=()=>{
+         const times=new Date().toLocaleTimeString();
+        return setTime(times);
+    };
+        
             useEffect(()=>{setInterval(()=>updateClock(),1000)},[])
   return (
-    <>{`${clock} ${tanggal()}`}</>
+    <>{`${time}  ${tanggal()}`}</>
   )
 }
