3914fc7565c9d3c242ca498ad817bcc5c22eac3d
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-10-08 12:36:43 +0700

    fix bug

--- src/components/organism/game/index.tsx
+++ src/components/organism/game/index.tsx
@@ -7,19 +7,24 @@ import {
 export default function Game() {
     let [score,setScore]=useState(0);
     let [status,setStatus]=useState('');
+    let [color,setColor]=useState('');
     useEffect(()=>{
             const statusUpdate=()=>{
                 if(score <= 200){
-                    setStatus('<span className="alert alert-danger">yowai mono!</span>')
+                    setStatus('yowai mono!')
+                    setColor('danger')
                 }
                 if(score >= 300){
-                    setStatus('<span className="alert alert-warning">not bad noobs!</span>')
+                    setStatus('not bad noobs!')
+                    setColor('primary')
                 }
                 if(score >= 500){
-                    setStatus('<span className="alert alert-info">not yet mature!</span>')
+                    setStatus('not yet mature!')
+                    setColor('info')
                 }
                 if(score >= 1000){
-                    setStatus('<span className="alert alert-success">you are sambal!</span>')
+                    setStatus('you are sambal!')
+                    setColor('success')
                 }
             }
             statusUpdate();
@@ -32,7 +37,7 @@ export default function Game() {
     <div className="d-flex flex-column-reverse flex-md-row justify-content-between">
         <div className="">
             <div className="container d-flex flex-column ">
-                <h5 className="mx-5 my-4 fs-5">
+                <h5 className={`mx-5 my-4 fs-5 text-${color}`}>
                     score:{`${score} ${status}`}
                 </h5>
                 <Button 
