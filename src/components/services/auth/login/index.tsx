8f32c894e625fa59d88f7cdfe3572e26bd39b700
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-10-13 16:20:17 +0700

    login page

--- src/components/services/auth/login/index.tsx
+++ src/components/services/auth/login/index.tsx
@@ -1,33 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import spesialForce from '../../../../imgs/spesial-force.jpg' 
 import './style.css'
 import {
+  Icon,
   Img
 }from '../../../assembleComponent'
 export default function Login() {
+  useEffect(()=>{
+    document.title='initer3737 | game'
+  },[])
   return (
     <div className="vw-100 vh-100 form-login">
-      {/* <Img 
-        src={spesialForce} 
-        alt={''} 
-        srcset={''} 
-        className={'img-fluid'} 
-        width={''} 
-        height={''} 
-        attr={undefined}/> */}
-     
         <div className="d-flex justify-content-center align-items-center position-absolute">
-          <div className="card bg-info opacity-form">
-            <div className="card-content">
-              <div className="card-header"></div>
-              <div className="card-body d-flex flex-column justify-content-center align-items-center gap-3">
-                <input type="text" />
-                <input type="text" />
+              <p className="color-1 h5 text-center">
+                <Icon variant={'info'} icon={'lightning'} name={' '}/>
+                counter strike
+              </p>
+              <p className="color-1 h5 text-center">
+                <Icon variant={'info'} icon={'box-arrow-right'} name={' '}/>
+                Login
+              </p>
+              
+                <input type="text" id='' placeholder='username'/>
+                <input type="text" id='' placeholder='password'/>
                 <button>submit</button>
-              </div>
-              <div className="card-footer"></div>
-            </div>
-          </div>
+              
+         
         </div>
 
     </div> //set is-login=true then navigate use to
