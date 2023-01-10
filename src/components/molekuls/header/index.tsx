c1119853010165ec8d46ba3023cf684598654b83
puthutwahyuaji <puthutwahyuaji13@gmail.com>
2023-01-11 00:46:07 +0700

    try css debug metode

--- src/components/molekuls/header/index.tsx
+++ src/components/molekuls/header/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Img} from '../../atom'
+import './header.css'
 import Banner from '../../../imgs/banner-982162_1920.jpg'
 export default function Header() {
   return (
-    <header className="row bg-4 text-light mb-3">
-         <Img src={`${Banner}`} alt="" className="row shadow" srcset={''} width={''} height={''} attr={[]}/> 
+    <header className="bg-4 text-light mb-3">
+         <Img src={`${Banner}`} alt="" className="shadow header__image" srcset={''} width={''} height={''} attr={[]}/> 
     </header> 
   )
 }
