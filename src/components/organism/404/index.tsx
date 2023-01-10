aa350370a7f8ba963a6d5457fa959c6a211eb240
puthutwahyuaji <puthutwahyuaji13@gmail.com>
2023-01-10 18:59:13 +0700

    nof found improve ui++

--- src/components/organism/404/index.tsx
+++ src/components/organism/404/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Logo from '../../../imgs/404.gif'
+import './notFound.css'
 export default function NotFound() {
     return(
-        <div className="">
-                <img src={Logo} alt="" className="w-50 h-100 d-none d-sm-none d-md-inline" />
-                <img src={Logo} alt="" className="col-12 col-md-6 h-100" />
+        <div className="not__found__container">
+                <img src={Logo} alt="" className="h-100 w-100" />
+                <div className="not__found__container__text">
+                <p className="">
+                    404 not found
+                </p>
+                </div>
         </div>
     )
 }
