06d7d20cfe2d988c159c785946c18c36c56c9d7a
puthutwahyuaji <puthutwahyuaji13@gmail.com>
2023-03-14 21:25:00 +0700

    update

--- src/components/organism/quotes/index.tsx
+++ src/components/organism/quotes/index.tsx
@@ -64,6 +64,16 @@ export default function Quotes() {
 
           <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
             <div className="text-center "><h5>
+              <Icon variant={''} icon={'pencil-square'} name={' '}/> 金</h5>
+            </div>
+            <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
+             kenapa tuan begitu rakus??, mencari harta kesana kemari,
+             tak peduli siang dan malam , apakah tuan akan menjadi kapitalis? yang hanya ada uang dikepala tuan ? dan tidak perduli kepada kehidupan setelah kematian???, esok tuan akan dimakamkan ,disana akan ditanya perkara yang tuan lewatkan semasa hidup, tentang kepatuhan kepada tuhan yang menciptakan malam ,apakah tuan tidak melihat ??
+            </div>
+          </div>
+
+          <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
+            <div className="text-center "><h5>
               <Icon variant={''} icon={'pencil-square'} name={' '}/> 夜</h5>
             </div>
             <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
