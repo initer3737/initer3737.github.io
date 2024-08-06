3557e84ee29c3c3be0c412c45f0d517fb5c07dc1
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2024-08-06 10:10:22 +0700

    update

--- src/components/organism/quotes/index.tsx
+++ src/components/organism/quotes/index.tsx
@@ -88,6 +88,15 @@ export default function Quotes() {
             </div>
           </div>
 
+          <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
+            <div className="text-center "><h5>
+              <Icon variant={''} icon={'pencil-square'} name={' '}/> semangat yamato</h5>
+            </div>
+            <div className="text-start fs-5 px-5 col-12 col-md-6  m-auto">
+             wibu imperialis berjalan dengan semangat yamato yang tak pernah padam
+            </div>
+          </div>
+
           {/* <div className="col-12 mt-5 m-auto bg-4 text-light mb-3 px-2 py-4">
             <div className="text-center "><h5>
               <Icon variant={''} icon={'pencil-square'} name={' '}/> родители</h5>
