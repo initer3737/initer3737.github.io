2bc494aa300b55041207c7f57c3491524c095157
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2024-08-12 17:39:28 +0700

    comrade vika gif

--- src/components/services/pwa/notification/index.tsx
+++ src/components/services/pwa/notification/index.tsx
@@ -1,7 +1,7 @@
 const ComradeVika=()=>{
      let  title= 'comrade Vika need you!!'
      let body=`serve to the motherland!!! \n call to all kombatans \n war event is on going!!`
-     let icon='/static/media/Vika.f704162606f0c53f30d7.gif'
+     let icon='/static/media/vika.2ed2ee9539a126820087.gif'
      return {
          title,
          body,
